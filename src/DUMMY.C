65f552e8e5ab0f68351ff07792646c2631ecf340
BenoitRen <benoitren@benoitren.be>
2023-08-19 21:22:32 +0200

    Added default arguments to pa_set().

--- src/DUMMY.C
+++ src/DUMMY.C
@@ -1,4 +1,4 @@
-// Addresses correspond to R7*.ELF, R8*.ELF
+#include "EQU.H"
 #include "DUMMY.H"
 
 
@@ -7,7 +7,7 @@
 
 
 
-void pa_set() {} /* Line 10, Address: 0x1013e20 */
+void pa_set(act_info* pActwk = 0, char value = 0) {} /* Line 10, Address: 0x1013e20 */
 
 
 
@@ -25,4 +25,3 @@ void bgmset(short wD0) { /* Line 24, Address: 0x1013e40 */
   wD0 = 0; /* Line 25, Address: 0x1013e48 */
 
 } /* Line 27, Address: 0x1013e4c */
-
