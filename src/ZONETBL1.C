4902c8f10cddf16e7526a3f8c612406dcce963f6
BenoitRen <benoitren@benoitren.be>
2023-08-19 23:16:42 +0200

    Added new types header. Fix errors caught during compilation.

--- src/ZONETBL1.C
+++ src/ZONETBL1.C
@@ -1,3 +1,5 @@
+#include "TYPES.H"
+
 unsigned short title_tbl[32] = {
    304,  552,  360,  346,
    256,  568,  376,  602,
@@ -9,58 +11,40 @@ unsigned short title_tbl[32] = {
    256,  464,  272, 2138
 };
 spr_array titlepat0 = {
-  .cnt = 1,
-  .spra = {
-    { -16, -112, 0, 349 }
-  }
+  1,
+  { { -16, -112, 0, 349 } }
 };
 spr_array titlepat1 = {
-  .cnt = 1,
-  .spra = {
-    { -24, -8, 0, 350 }
-  }
+  1,
+  { { -24, -8, 0, 350 } }
 };
 spr_array titlepat2 = {
-  .cnt = 1,
-  .spra = {
-    { -4, -24, 0, 351 }
-  }
+  1,
+  { { -4, -24, 0, 351 } }
 };
 spr_array titlepat3 = {
-  .cnt = 1,
-  .spra = {
-    { -80, -8, 0, 352 }
-  }
+  1,
+  { { -80, -8, 0, 352 } }
 };
 spr_array titlepat4 = {
-  .cnt = 1,
-  .spra = {
-    { -24, -24, 0, 353 }
-  }
+  1,
+  { { -24, -24, 0, 353 } }
 };
 spr_array titlepat5 = {
-  .cnt = 1,
-  .spra = {
-    { -24, -24, 0, 354 }
-  }
+  1,
+  { { -24, -24, 0, 354 } }
 };
 spr_array titlepat6 = {
-  .cnt = 1,
-  .spra = {
-    { -24, -24, 0, 355 }
-  }
+  1,
+  { { -24, -24, 0, 355 } }
 };
 spr_array titlepat7 = {
-  .cnt = 1,
-  .spra = {
-    { 0, -56, 0, 356 }
-  }
+  1,
+  { { 0, -56, 0, 356 } }
 };
 spr_array titlepat8 = {
-  .cnt = 1,
-  .spra = {
-    { 0, 0, 0, 357 }
-  }
+  1,
+  { { 0, 0, 0, 357 } }
 };
 spr_array* title_pat[9] = {
   &titlepat0,
