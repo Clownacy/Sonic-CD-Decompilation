d899fd7917c09b3622e65b278af9d880dbdd001f
BenoitRen <benoitren@benoitren.be>
2024-03-06 14:06:58 +0100

    Initial commit of R4.

---
+++ src/R4/EDTBL41C.C
@@ -0,0 +1,55 @@
+#include "..\TYPES.H"
+
+sprite_pattern* harir4pat[];
+sprite_pattern* renketu4pat[];
+sprite_pattern* pat_tekkyu4[];
+sprite_pattern* pat_screw0[];
+sprite_pattern* pat_screw1[];
+sprite_pattern* awapat[];
+sprite_pattern* springpat[];
+sprite_pattern* spring90pat[];
+sprite_pattern* pat_wall1[];
+sprite_pattern* pat_tonbo_e[];
+sprite_pattern* pat_tonbo_b[];
+sprite_pattern* pat_amenbo_e[];
+sprite_pattern* pat_amenbo_b[];
+sprite_pattern* pat_tagameb_e[];
+sprite_pattern* pat_tagameb_b[];
+sprite_pattern* pat_yago_e[];
+sprite_pattern* pat_yago_b[];
+sprite_pattern* pat_friend0[];
+sprite_pattern* pat_friend1[];
+sprite_pattern* itempat[];
+sprite_pattern* boupat[];
+
+edit_info_array edit_tbl = {
+  25,
+  0,
+  {
+    { 34, 1, harir4pat,       928 },
+    { 62, 1, renketu4pat,   17216 },
+    { 56, 1, pat_tekkyu4,     880 },
+    { 53, 1, pat_screw0,    17280, 128, 0, 0, 0 },
+    { 53, 1, pat_screw0,    17280, 129, 3, 0, 0 },
+    { 53, 1, pat_screw1,    17280, 130, 0, 0, 0 },
+    { 53, 1, pat_screw1,    17280, 131, 3, 0, 0 },
+    { 32, 4, awapat,        33930, 130, 0, 0, 0 },
+    { 10, 1, springpat,      1312 },
+    { 10, 1, springpat,      1312, 0, 2, 0, 0 },
+    { 10, 1, spring90pat,    1312, 4, 0, 0, 0 },
+    { 10, 1, spring90pat,    1312, 4, 1, 0, 0 },
+    { 52, 1, pat_wall1,     17514 },
+    { 43, 1, pat_tonbo_e,    9238 },
+    { 43, 1, pat_tonbo_b,    9238, 1, 0, 0, 0 },
+    { 44, 1, pat_amenbo_e,   9136 },
+    { 44, 1, pat_amenbo_b,   9136, 1, 0, 0, 0 },
+    { 45, 1, pat_tagameb_e,  9168 },
+    { 45, 1, pat_tagameb_b,  9168, 1, 0, 0, 0 },
+    { 46, 1, pat_yago_e,     9200 },
+    { 46, 1, pat_yago_b,     9200, 1, 0, 0, 0 },
+    { 47, 1, pat_friend0,    9296 },
+    { 47, 1, pat_friend1,    9296, 1, 0, 0, 1 },
+    { 25, 4, itempat,        1448 },
+    {  5, 4, boupat,          760 }
+  }
+};
---
+++ src/R4/WALL1.C
@@ -0,0 +1,138 @@
+#include "..\EQU.H"
+#include "WALL1.H"
+
+void act_init(sprite_status* pActwk);
+void act_move(sprite_status* pActwk);
+
+static sprite_pattern pat00 = {
+  1,
+  { { -16, -16, 0, 450 } }
+};
+static sprite_pattern pat01 = {
+  1,
+  { { -16, -16, 0, 451 } }
+};
+static sprite_pattern pat02 = {
+  1,
+  { { -32, -16, 0, 452 } }
+};
+static sprite_pattern pat03 = {
+  1,
+  { { -16, -32, 0, 453 } }
+};
+static sprite_pattern pat04 = {
+  1,
+  { { -32, -32, 0, 454 } }
+};
+static sprite_pattern pat05 = {
+  1,
+  { { -64, -16, 0, 455 } }
+};
+static sprite_pattern pat06 = {
+  1,
+  { { -16, -64, 0, 456 } }
+};
+sprite_pattern* pat_wall1[7] = {
+  &pat01,
+  &pat02,
+  &pat03,
+  &pat04,
+  &pat05,
+  &pat06
+};
+static unsigned char tbl0[14] = {
+  16, 16,
+  16, 16,
+  32, 16,
+  16, 32,
+  32, 32,
+  64, 16,
+  16, 64
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+void wall1(sprite_status* pActwk) { /* Line 105, Address: 0x101e0e0 */
+  if (pActwk->r_no0) { /* Line 106, Address: 0x101e0ec */
+    act_move(pActwk); /* Line 107, Address: 0x101e0fc */
+  } else { /* Line 108, Address: 0x101e108 */
+    act_init(pActwk); /* Line 109, Address: 0x101e110 */
+  }
+} /* Line 111, Address: 0x101e11c */
+
+
+
+
+
+static void act_init(sprite_status* pActwk) { /* Line 117, Address: 0x101e130 */
+  pActwk->r_no0 += 2; /* Line 118, Address: 0x101e13c */
+  pActwk->actflg |= 4; /* Line 119, Address: 0x101e14c */
+  pActwk->sprpri = 1; /* Line 120, Address: 0x101e15c */
+  pActwk->sproffset = 17514; /* Line 121, Address: 0x101e168 */
+  pActwk->patbase = pat_wall1; /* Line 122, Address: 0x101e174 */
+  pActwk->patno = pActwk->userflag.b.h; /* Line 123, Address: 0x101e184 */
+  pActwk->sprhs = pActwk->sprhsize = tbl0[pActwk->userflag.b.h * 2]; /* Line 124, Address: 0x101e194 */
+  pActwk->sprvsize = tbl0[pActwk->userflag.b.h * 2 + 1]; /* Line 125, Address: 0x101e1c8 */
+
+  act_move(pActwk); /* Line 127, Address: 0x101e1f8 */
+} /* Line 128, Address: 0x101e204 */
+
+
+
+
+
+static void act_move(sprite_status* pActwk) { /* Line 134, Address: 0x101e220 */
+  hitchk(pActwk, &actwk[0]); /* Line 135, Address: 0x101e22c */
+  actionsub(pActwk); /* Line 136, Address: 0x101e240 */
+  frameout_s(pActwk); /* Line 137, Address: 0x101e24c */
+} /* Line 138, Address: 0x101e258 */
