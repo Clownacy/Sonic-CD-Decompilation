8083d31f16cd7b27994b8d962ff1ca16773170d8
Clownacy <Clownacy@users.noreply.github.com>
2024-11-09 03:58:45 +0000

    Add basic level drawing.

--- src/LAUNCHER/MAIN.C
+++ src/LAUNCHER/MAIN.C
@@ -10,6 +10,9 @@
 
 #define COUNT_OF(x) (sizeof(x) / sizeof(x[0]))
 
+#define SCREEN_WIDTH 320
+#define SCREEN_HEIGHT 224
+
 extern dlink_export ExportedFunctions;
 
 static unsigned char buffers[10][1024*1024*10];
@@ -20,12 +23,22 @@ static SDL_Window *window;
 static SDL_Palette *palette;
 static SDL_Surface *framebuffer;
 
+// TODO: Dynamically allocate based on what the file says the number of sprites are?
 static SDL_Surface *sprites[700][2][2];
+
+// TODO: Dynamically allocate based on what the file says the number of tiles are?
+static SDL_Surface *tiles[0x800];
 
 static game_info state;
 
-static int SetGrid(int, int, int, int)
+typedef unsigned short Block;
+
+static Block planes[2][32][64];
+
+static int SetGrid(const int plane, const int x, const int y, const int block, const int flip)
 {
+	// TODO: Flip.
+	planes[plane][y][x] = block;
 	return 0;
 }
 
@@ -286,11 +299,95 @@ static bool LoadSprites(const char* const path)
 	return true;
 }
 
+static bool LoadTiles(const char* const path)
+{
+	FILE *file = fopen(path, "rb");
+
+	if (file == NULL)
+		return false;
+
+	fseek(file, 8, SEEK_SET);
+
+	const unsigned long total_tiles = ReadU32LE(file);
+	const unsigned long tiles_offset = ReadU32LE(file);
+
+	fseek(file, tiles_offset, SEEK_SET);
+
+	const unsigned int padded_width = 8;
+	const unsigned int height = 8;
+	const int total_pixel_bytes = padded_width * height / 2;
+
+	for (unsigned long i = 0; i < total_tiles; ++i)
+	{
+		SDL_Surface* const sprite = CreateSpriteSurface(padded_width, height);
+
+		if (sprite != NULL)
+		{
+			unsigned char *pixels = sprite->pixels;
+
+			for (int j = 0; j < total_pixel_bytes; ++j)
+			{
+				const unsigned char packed_pixels = fgetc(file);
+
+				// Redirect all transparent pixels to colour 0.
+				*pixels = (packed_pixels >> 4);
+				*pixels = *pixels % 0x10 == 0 ? 0 : *pixels;
+				++pixels;
+
+				*pixels = (packed_pixels & 0xF);
+				*pixels = *pixels % 0x10 == 0 ? 0 : *pixels;
+				++pixels;
+			}
+		}
+
+		tiles[i] = sprite;
+	}
+
+	fclose(file);
+
+	return true;
+}
+
+static void DrawPlanes(void)
+{
+	const unsigned long vscroll = ExportedFunctions.Get_vscroll();
+	unsigned int vscrolls[2];
+	vscrolls[0] = vscroll >> 16;
+	vscrolls[1] = vscroll & 0xFFFF;
+
+	unsigned int hscrolls[2];
+	hscrolls[0] = ExportedFunctions.Get_scra_h_posiw() >> 16;
+	hscrolls[1] = ExportedFunctions.Get_scrb_h_posiw() >> 16;
+
+	for (unsigned int plane_inv = 0; plane_inv < COUNT_OF(planes); ++plane_inv)
+	{
+		const unsigned int plane = plane_inv ^ 1;
+
+		for (unsigned int y = 0; y < (SCREEN_HEIGHT + (8 - 1)) / 8 + 1; ++y)
+		{
+			for (unsigned int x = 0; x < (SCREEN_WIDTH + (8 - 1)) / 8 + 1; ++x)
+			{
+				SDL_Rect destination_rectangle;
+				destination_rectangle.x = -(hscrolls[plane] % 8) + x * 8;
+				destination_rectangle.y = -(vscrolls[plane] % 8) + y * 8;
+				destination_rectangle.w = 8;
+				destination_rectangle.h = 8;
+
+				const unsigned int src_x = (hscrolls[plane] / 8 + x) % COUNT_OF(planes[plane][y]);
+				const unsigned int src_y = (vscrolls[plane] / 8 + y) % COUNT_OF(planes[plane]);
+
+				if (SDL_BlitSurface(tiles[planes[plane][src_y][src_x] & 0x7FF], NULL, framebuffer, &destination_rectangle) == -1)
+					fputs("Failed to bit to framebuffer surface.\n", stderr);
+			}
+		}
+	}
+}
+
 int SDL_main(const int argc, char** const argv)
 {
 	SDL_Init(SDL_INIT_VIDEO | SDL_INIT_EVENTS);
 
-	window = SDL_CreateWindow("Sonic CD", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 320, 224, 0);
+	window = SDL_CreateWindow("Sonic CD", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, 0);
 
 	if (window == NULL)
 	{
@@ -306,7 +403,7 @@ int SDL_main(const int argc, char** const argv)
 		}
 		else
 		{
-			framebuffer = SDL_CreateRGBSurfaceWithFormat(0, 320, 224, 0, SDL_PIXELFORMAT_INDEX8);
+			framebuffer = SDL_CreateRGBSurfaceWithFormat(0, SCREEN_WIDTH, SCREEN_HEIGHT, 0, SDL_PIXELFORMAT_INDEX8);
 
 			if (framebuffer == NULL)
 			{
@@ -374,60 +471,69 @@ int SDL_main(const int argc, char** const argv)
 				}
 				else
 				{
-					ExportedFunctions.game_init();
-
-					bool alive = true;
-					while (alive)
+					if (!LoadTiles("R1/11A/TCMP11A.CMP"))
 					{
-						SDL_Event event;
-						while (SDL_PollEvent(&event))
+						fputs("Failed to load sprites.\n", stderr);
+					}
+					else
+					{
+						ExportedFunctions.game_init();
+
+						bool alive = true;
+						while (alive)
 						{
-							switch (event.type)
+							SDL_Event event;
+							while (SDL_PollEvent(&event))
 							{
-								case SDL_QUIT:
-									alive = false;
-									break;
+								switch (event.type)
+								{
+									case SDL_QUIT:
+										alive = false;
+										break;
+								}
 							}
+
+							const Uint8* const keyboard_state = SDL_GetKeyboardState(NULL);
+							unsigned short swdata = 0;
+							if (keyboard_state[SDL_SCANCODE_SPACE])
+								swdata |= 0x10;
+							if (keyboard_state[SDL_SCANCODE_UP])
+								swdata |= 0x100;
+							if (keyboard_state[SDL_SCANCODE_DOWN])
+								swdata |= 0x200;
+							if (keyboard_state[SDL_SCANCODE_LEFT])
+								swdata |= 0x400;
+							if (keyboard_state[SDL_SCANCODE_RIGHT])
+								swdata |= 0x800;
+							ExportedFunctions.SWdataSet(swdata, 0);
+
+							// Clear framebuffer.
+							SDL_FillRect(framebuffer, NULL, 0);
+
+							DrawPlanes();
+
+							ExportedFunctions.game();
+
+							// Update the colour palette.
+							for (unsigned int i = 0; i < COUNT_OF(raw_palettes[1]); ++i)
+							{
+								SDL_Color colour;
+								colour.r = raw_palettes[1][i].peRed;
+								colour.g = raw_palettes[1][i].peGreen;
+								colour.b = raw_palettes[1][i].peBlue;
+								colour.a = 0xFF;
+
+								if (SDL_SetPaletteColors(palette, &colour, i, 1) < 0)
+									fputs("Failed to set framebuffer palette colour.\n", stderr);
+							}
+
+							if (SDL_BlitSurface(framebuffer, NULL, SDL_GetWindowSurface(window), NULL) == -1)
+								fputs("Failed to blit to window surface.\n", stderr);
+
+							SDL_UpdateWindowSurface(window);
+							const int frames_per_second = 60;
+							SDL_Delay((1000 + (frames_per_second / 2)) / frames_per_second);
 						}
-
-						const Uint8* const keyboard_state = SDL_GetKeyboardState(NULL);
-						unsigned short swdata = 0;
-						if (keyboard_state[SDL_SCANCODE_SPACE])
-							swdata |= 0x10;
-						if (keyboard_state[SDL_SCANCODE_UP])
-							swdata |= 0x100;
-						if (keyboard_state[SDL_SCANCODE_DOWN])
-							swdata |= 0x200;
-						if (keyboard_state[SDL_SCANCODE_LEFT])
-							swdata |= 0x400;
-						if (keyboard_state[SDL_SCANCODE_RIGHT])
-							swdata |= 0x800;
-						ExportedFunctions.SWdataSet(swdata, 0);
-
-						// Clear framebuffer.
-						SDL_FillRect(framebuffer, NULL, 0);
-
-						ExportedFunctions.game();
-
-						// Update the colour palette.
-						for (unsigned int i = 0; i < COUNT_OF(raw_palettes[1]); ++i)
-						{
-							SDL_Color colour;
-							colour.r = raw_palettes[1][i].peRed;
-							colour.g = raw_palettes[1][i].peGreen;
-							colour.b = raw_palettes[1][i].peBlue;
-							colour.a = 0xFF;
-
-							if (SDL_SetPaletteColors(palette, &colour, i, 1) < 0)
-								fputs("Failed to set framebuffer palette colour.\n", stderr);
-						}
-
-						if (SDL_BlitSurface(framebuffer, NULL, SDL_GetWindowSurface(window), NULL) == -1)
-							fputs("Failed to blit to window surface.\n", stderr);
-
-						SDL_UpdateWindowSurface(window);
-						const int frames_per_second = 60;
-						SDL_Delay((1000 + (frames_per_second / 2)) / frames_per_second);
 					}
 				}
 
