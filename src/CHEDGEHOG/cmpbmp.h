afb1f5a9c29ab7d41ea392392c59834c7512292a
BenoitRen <benoitren@benoitren.be>
2024-12-20 22:22:02 +0100

    CHEDGEHOG: Refactored cmpbmp. Removed SDL dependency.

--- src/CHEDGEHOG/cmpbmp.h
+++ src/CHEDGEHOG/cmpbmp.h
@@ -1,9 +1,15 @@
 #ifndef CMPBMP_H
 #define CMPBMP_H
 
-int load_sprites(char* p_filename);
-void unload_sprites();
-int load_tiles(char* p_filename);
-void unload_tiles();
+#include "cmpbmpheader.h"
+#include "cmpspritemeta.h"
+#include "cmptilemeta.h"
+#include "extractedbitmap.h"
+
+unsigned char* read_cmp_bmp_header(unsigned char* p_bytes, cmp_bmp_header* header);
+unsigned char* read_cmp_sprite_meta(unsigned char* p_bytes, cmp_sprite_meta** pp_meta, unsigned long sprite_cnt);
+unsigned char* read_cmp_tile_meta(unsigned char* p_bytes, cmp_tile_meta* p_meta, unsigned long tile_cnt);
+void extract_sprites(unsigned char* p_output, extracted_bitmap* p_extracted, unsigned char* p_input, unsigned long cnt, cmp_sprite_meta* p_meta);
+void extract_tiles(unsigned char* p_output, extracted_bitmap* p_extracted, unsigned char* p_input, unsigned long cnt, cmp_tile_meta meta);
 
 #endif
---
+++ src/CHEDGEHOG/extractedbitmap.h
@@ -0,0 +1,11 @@
+#ifndef EXTRACTEDBITMAP_H
+#define EXTRACTEDBITMAP_H
+
+typedef struct extracted_bitmap {
+  unsigned char* p_data;
+  unsigned short width;
+  unsigned short height;
+}
+extracted_bitmap;
+
+#endif
