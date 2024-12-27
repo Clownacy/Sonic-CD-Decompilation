0c78aeaf3d034b5b69df3694ecab3d02d354aee1
BenoitRen <benoitren@benoitren.be>
2024-12-27 10:30:42 +0100

    CHEDGEHOG: Split blit_planes into blit_lo_planes and blit_hi_planes.

--- src/CHEDGEHOG/tiles.c
+++ src/CHEDGEHOG/tiles.c
@@ -203,9 +203,13 @@ void SetGrid(int base, int x, int y, int block, int frip) {
 }
 
 
-void blit_planes(unsigned char* p_pixelbuffer, unsigned short* p_hscroll_a, unsigned short vscroll_a, unsigned short* p_hscroll_b, unsigned short vscroll_b) {
+void blit_lo_planes(unsigned char* p_pixelbuffer, unsigned short* p_hscroll_a, unsigned short vscroll_a, unsigned short* p_hscroll_b, unsigned short vscroll_b) {
   blit_plane(p_pixelbuffer, g_plane_b, p_hscroll_b, vscroll_b);
   blit_plane(p_pixelbuffer, g_plane_a_lo, p_hscroll_a, vscroll_a);
+}
+
+
+void blit_hi_planes(unsigned char* p_pixelbuffer, unsigned short* p_hscroll_a, unsigned short vscroll_a, unsigned short* p_hscroll_b, unsigned short vscroll_b) {
   blit_plane(p_pixelbuffer, g_plane_a_hi, p_hscroll_a, vscroll_a);
 }
 
--- src/CHEDGEHOG/tiles.h
+++ src/CHEDGEHOG/tiles.h
@@ -4,6 +4,7 @@
 int load_tile_bitmaps(char* p_filename);
 void unload_tile_bitmaps();
 void SetGrid(int base, int x, int y, int block, int frip);
-void blit_planes(unsigned char* p_pixelbuffer, unsigned short* p_hscroll_a, unsigned short vscroll_a, unsigned short* p_hscroll_b, unsigned short vscroll_b);
+void blit_lo_planes(unsigned char* p_pixelbuffer, unsigned short* p_hscroll_a, unsigned short vscroll_a, unsigned short* p_hscroll_b, unsigned short vscroll_b);
+void blit_hi_planes(unsigned char* p_pixelbuffer, unsigned short* p_hscroll_a, unsigned short vscroll_a, unsigned short* p_hscroll_b, unsigned short vscroll_b);
 
 #endif
