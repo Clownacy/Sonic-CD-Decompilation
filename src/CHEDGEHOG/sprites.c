462e59ff09d1204d937232686f592a54d5003df6
BenoitRen <benoitren@benoitren.be>
2024-12-27 11:30:25 +0100

    CHEDGEHOG: Refactored the four blit_sprite functions into one.

--- src/CHEDGEHOG/sprites.c
+++ src/CHEDGEHOG/sprites.c
@@ -8,10 +8,7 @@
 #include "extractedbitmap.h"
 #include "spriteinfo.h"
 #include "szdd.h"
-static void blit_sprite(unsigned char* p_pixelbuffer, extracted_bitmap bitmap, sprite_info info);
-static void blit_sprite_hflip(unsigned char* p_pixelbuffer, extracted_bitmap bitmap, sprite_info info);
-static void blit_sprite_vflip(unsigned char* p_pixelbuffer, extracted_bitmap bitmap, sprite_info info);
-static void blit_sprite_hvflip(unsigned char* p_pixelbuffer, extracted_bitmap bitmap, sprite_info info);
+static void blit_sprite(unsigned char* p_pixelbuffer, extracted_bitmap bitmap, int x, int y, int h_dir, int v_dir);
 
 static unsigned char* g_sprite_bitmap_data = 0;
 static extracted_bitmap g_sprite_bitmaps[SPRITE_BITMAPS_MAX] = { 0 };
@@ -95,131 +92,18 @@ void blit_sprites(unsigned char* p_pixelbuffer) {
       extracted_bitmap bitmap = g_sprite_bitmaps[info.index];
 
       switch (g_sprites[i].reverse & FLIP_MASK) {
-        case NOFLIP: blit_sprite(p_pixelbuffer, bitmap, info); break;
-        case HFLIP: blit_sprite_hflip(p_pixelbuffer, bitmap, info); break;
-        case VFLIP: blit_sprite_vflip(p_pixelbuffer, bitmap, info); break;
-        case HVFLIP: blit_sprite_hvflip(p_pixelbuffer, bitmap, info); break;
+        case NOFLIP: blit_sprite(p_pixelbuffer, bitmap, info.x, info.y, 1, 1); break;
+        case HFLIP: blit_sprite(p_pixelbuffer, bitmap, info.x, info.y, -1, 1); break;
+        case VFLIP: blit_sprite(p_pixelbuffer, bitmap, info.x, info.y, 1, -1); break;
+        case HVFLIP: blit_sprite(p_pixelbuffer, bitmap, info.x, info.y, -1, -1); break;
       }
     }
   }
 }
 
 
-static void blit_sprite(unsigned char* p_pixelbuffer, extracted_bitmap bitmap, sprite_info info) {
-  int x = info.x;
+static void blit_sprite(unsigned char* p_pixelbuffer, extracted_bitmap bitmap, int x, int y, int h_dir, int v_dir) {
   int x_end = x + bitmap.width;
-  int y = info.y;
-  int y_end = info.y + bitmap.height;
-  int x_skip = 0;
-
-  if (x >= SCREEN_WIDTH) return;
-  if (x_end <= 0) return;
-  if (y >= SCREEN_HEIGHT) return;
-  if (y_end <= 0) return;
-
-  if (x < 0) {
-    x_skip = -x;
-    x = 0;
-  }
-  if (x_end > SCREEN_WIDTH) {
-    x_end = SCREEN_WIDTH;
-  }
-  if (y < 0) {
-    int i;
-
-    for (i = 0; i > y; --i) {
-      bitmap.p_data += bitmap.width;
-    }
-
-    y = 0;
-  }
-  if (y_end > SCREEN_HEIGHT) {
-    y_end = SCREEN_HEIGHT;
-  }
-
-  p_pixelbuffer += y * SCREEN_WIDTH;
-
-  do {
-    unsigned char* p_pixelbuffer_copy = p_pixelbuffer + x;
-    unsigned char* p_bitmap_data_copy = bitmap.p_data + x_skip;
-    int i;
-
-    for (i = x; i < x_end; ++i) {
-      if (*p_bitmap_data_copy != SPRITE_TRANSPARENT_COLOR) {
-        *p_pixelbuffer_copy = *p_bitmap_data_copy;
-      }
-      ++p_pixelbuffer_copy;
-      ++p_bitmap_data_copy;
-    }
-
-    p_pixelbuffer += SCREEN_WIDTH;
-    bitmap.p_data += bitmap.width;
-    ++y;
-  }
-  while (y < y_end);
-}
-
-
-static void blit_sprite_hflip(unsigned char* p_pixelbuffer, extracted_bitmap bitmap, sprite_info info) {
-  int x = info.x;
-  int x_end = x + bitmap.width;
-  int y = info.y;
-  int y_end = info.y + bitmap.height;
-  int x_skip = 0;
-
-  if (x >= SCREEN_WIDTH) return;
-  if (x_end <= 0) return;
-  if (y >= SCREEN_HEIGHT) return;
-  if (y_end <= 0) return;
-
-  if (x < 0) {
-    x_skip = x;
-    x = 0;
-  }
-  if (x_end > SCREEN_WIDTH) {
-    x_end = SCREEN_WIDTH;
-  }
-  if (y < 0) {
-    int i;
-
-    for (i = 0; i > y; --i) {
-      bitmap.p_data += bitmap.width;
-    }
-
-    y = 0;
-  }
-  if (y_end > SCREEN_HEIGHT) {
-    y_end = SCREEN_HEIGHT;
-  }
-
-  p_pixelbuffer += y * SCREEN_WIDTH;
-  bitmap.p_data += bitmap.width - 1;
-
-  do {
-    unsigned char* p_pixelbuffer_copy = p_pixelbuffer + x;
-    unsigned char* p_bitmap_data_copy = bitmap.p_data + x_skip;
-    int i;
-
-    for (i = x; i < x_end; ++i) {
-      if (*p_bitmap_data_copy != SPRITE_TRANSPARENT_COLOR) {
-        *p_pixelbuffer_copy = *p_bitmap_data_copy;
-      }
-      ++p_pixelbuffer_copy;
-      --p_bitmap_data_copy;
-    }
-
-    p_pixelbuffer += SCREEN_WIDTH;
-    bitmap.p_data += bitmap.width;
-    ++y;
-  }
-  while (y < y_end);
-}
-
-
-static void blit_sprite_vflip(unsigned char* p_pixelbuffer, extracted_bitmap bitmap, sprite_info info) {
-  int x = info.x;
-  int x_end = x + bitmap.width;
-  int y = info.y;
   int y_end = y + bitmap.height;
   int x_skip = 0;
 
@@ -229,7 +113,7 @@ static void blit_sprite_vflip(unsigned char* p_pixelbuffer, extracted_bitmap bit
   if (y_end <= 0) return;
 
   if (x < 0) {
-    x_skip = -x;
+    x_skip = -x * h_dir;
     x = 0;
   }
   if (x_end > SCREEN_WIDTH) {
@@ -249,7 +133,14 @@ static void blit_sprite_vflip(unsigned char* p_pixelbuffer, extracted_bitmap bit
   }
 
   p_pixelbuffer += y * SCREEN_WIDTH;
-  bitmap.p_data += bitmap.width * (bitmap.height - 1);
+  if (h_dir == -1) {
+    // Move to the last pixel of the line.
+    bitmap.p_data += bitmap.width - 1;
+  }
+  if (v_dir == -1) {
+    // Move to the last line.
+    bitmap.p_data += bitmap.width * (bitmap.height - 1);
+  }
 
   do {
     unsigned char* p_pixelbuffer_copy = p_pixelbuffer + x;
@@ -261,67 +152,11 @@ static void blit_sprite_vflip(unsigned char* p_pixelbuffer, extracted_bitmap bit
         *p_pixelbuffer_copy = *p_bitmap_data_copy;
       }
       ++p_pixelbuffer_copy;
-      ++p_bitmap_data_copy;
+      p_bitmap_data_copy += h_dir;
     }
 
     p_pixelbuffer += SCREEN_WIDTH;
-    bitmap.p_data -= bitmap.width;
-    ++y;
-  }
-  while (y < y_end);
-}
-
-
-static void blit_sprite_hvflip(unsigned char* p_pixelbuffer, extracted_bitmap bitmap, sprite_info info) {
-  int x = info.x;
-  int x_end = x + bitmap.width;
-  int y = info.y;
-  int y_end = y + bitmap.height;
-  int x_skip = 0;
-
-  if (x >= SCREEN_WIDTH) return;
-  if (x_end <= 0) return;
-  if (y >= SCREEN_HEIGHT) return;
-  if (y_end <= 0) return;
-
-  if (x < 0) {
-    x_skip = x;
-    x = 0;
-  }
-  if (x_end > SCREEN_WIDTH) {
-    x_end = SCREEN_WIDTH;
-  }
-  if (y < 0) {
-    int i;
-
-    for (i = 0; i > y; --i) {
-      bitmap.p_data += bitmap.width;
-    }
-
-    y = 0;
-  }
-  if (y_end > SCREEN_HEIGHT) {
-    y_end = SCREEN_HEIGHT;
-  }
-
-  p_pixelbuffer += y * SCREEN_WIDTH;
-  bitmap.p_data += bitmap.width * bitmap.height - 1;
-
-  do {
-    unsigned char* p_pixelbuffer_copy = p_pixelbuffer + x;
-    unsigned char* p_bitmap_data_copy = bitmap.p_data + x_skip;
-    int i;
-
-    for (i = x; i < x_end; ++i) {
-      if (*p_bitmap_data_copy != SPRITE_TRANSPARENT_COLOR) {
-        *p_pixelbuffer_copy = *p_bitmap_data_copy;
-      }
-      ++p_pixelbuffer_copy;
-      --p_bitmap_data_copy;
-    }
-
-    p_pixelbuffer += SCREEN_WIDTH;
-    bitmap.p_data -= bitmap.width;
+    bitmap.p_data += bitmap.width * v_dir;
     ++y;
   }
   while (y < y_end);
